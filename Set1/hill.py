d63bf11562e089aa01fb942d4ebdda15b5af506c
Alex Foglia <alex.f1793@gmail.com>
2019-03-29 05:06:20 +0100

    finiti

--- Set1/hill.py
+++ Set1/hill.py
@@ -1,48 +1,117 @@
-from hilltools import *
+#This is the code of Exercise 3.2
+#It depends upon library numpy, available with pip: launch pip install numpy if missing
+#In order to execute the code, user has to launch python3 hill.py
+#
+#Author: Alex Foglia, 6336805
 
-def create_indexes(block):
+import numpy
+import math
+from numpy import matrix
+from numpy import linalg
+from itertools import *
+
+gamma = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+def indexof(letter): #utility function used to get the index of a letter within the alphabet gamma
+    for i in range(0,len(gamma)):
+        if letter == gamma[i]:
+            return i
+    return -1
+
+def clean(text): #utility function used to clean a text as requested
+    cln = text.lower()
+    for ch in text:
+        if ch not in gamma:
+            cln = cln.replace(ch,'')
+    return cln
+
+def dot_product(mat, vec): #compute the dot product between a matrix and a vector
+    p = len(gamma)
+    res = []
+    for row in mat:
+        if len(row)!=len(vec):
+            raise ValueError("Size of mat is different from size of vec")
+        res_i = 0
+        for i in range(0,len(row)):
+            res_i = res_i + row[i]*vec[i]
+        res.append(res_i%p)
+    return res
+
+def modMatInv(A,p):       # Finds the inverse of matrix A mod p
+    n=len(A)
+    A=matrix(A)
+    adj=numpy.zeros(shape=(n,n))
+    for i in range(0,n):
+      for j in range(0,n):
+        adj[i][j]=((-1)**(i+j)*int(round(linalg.det(minor(A,j,i)))))%p
+    return (__modInv__(int(round(linalg.det(A))),p)*adj)%p
+
+def __modInv__(a,p):          # Finds the inverse of a mod p, if it exists (note that a is a number)
+    for i in range(1,p):
+      if (i*a)%p==1:
+        return i
+    raise ValueError(str(a)+" has no inverse mod "+str(p))
+
+
+def minor(A,i,j):    # Return matrix A with the ith row and jth column deleted
+    A=numpy.array(A) # Convert A to numpy.array class
+    minor=numpy.zeros(shape=(len(A)-1,len(A)-1)) #constructing minor matrix
+    p=0
+    for s in range(0,len(minor)):
+      if p==i:
+          p=p+1
+      q=0
+      for t in range(0,len(minor)):
+          if q==j:
+              q=q+1
+          minor[s][t]=A[p][q]
+          q=q+1
+      p=p+1
+    return minor
+
+def create_indexes(block): #create a vector of index foreach block
     indexes = []
     for ch in block:
         indexes.append(indexof(ch))
     return indexes
 
-def create_characters(ciphervalues):
+def create_characters(ciphervalues): #from a block of index it creates a vector of characters
     chars = []
     for val in ciphervalues:
         chars.append(gamma[int(val)])
     return ''.join(chars)
 
-def cipher_block(plainblock, key):
-    blockvalues = create_indexes(plainblock)
-    ciphervalues = dot_product(key, blockvalues)
-    cipherblock = create_characters(ciphervalues)
+def cipher_block(plainblock, key): #function which encrypt a block
+    blockvalues = create_indexes(plainblock) #create index value of characters
+    ciphervalues = dot_product(key, blockvalues) #compute dot product between plaintext indexes and the key
+    cipherblock = create_characters(ciphervalues) #get back to ciphered characters
     return cipherblock
 
-def encrypt(text, key):
-    text = clean(text)
+def encrypt(text, key): #function which encrypt a text
+    text = clean(text) #clean text
     m = len(key)
     blocks = []
     cipherblocks = []
-    for i in range(0,len(text),m):
+    for i in range(0,len(text),m): #we divide text in block of length equal to the key size
         blocks.append(text[i:i+m])
-    if len(blocks[-1])!= m:
+    if len(blocks[-1])!= m: #padding last block if it is too short
         for i in range(len(blocks[-1]),m):
             blocks[-1] = blocks[-1] + 'a'
-    for block in blocks:
+    for block in blocks: #foreach block it constructs a ciphered message composed of the result of ciphering each block with the same key
         cipherblocks.append(cipher_block(block,key))
     return ''.join(cipherblocks)
 
-def decrypt(text, key):
+def decrypt(text, key): #just an invocation of encrypt with the inverse key matrix
     key = modMatInv(key, len(gamma))
     return encrypt(text,key)
 
-def shiftLeft(v):
+def shiftLeft(v): #compute the left shift of a vector
     temp = v[0]
     for i in range(0,len(v)-1):
         v[i] = v[i+1]
     v[-1] = temp
 
-def find_key(pt, ct, m): #pt,ct shall be list of indexes
+def find_key(pt, ct, m): #Crack hill cipher: pt,ct shall be list of indexes, m is the key size
     pairs = []
     for i in range(0,len(pt),m):
         pairs.append((create_indexes(pt[i:i+m]),create_indexes(ct[i:i+m]))) #pairs will be a list of pairs (plaintext,ciphertext)
@@ -56,15 +125,15 @@ def find_key(pt, ct, m): #pt,ct shall be list of indexes
             pstar.append(pairs[i][0]) #P* is a m x m matrix in which each line is known plaintext
             cstar.append(pairs[i][1]) #C* is a m x m matrix in which cstar[i] = ciphertext(plaintext[i])
         try:
-            key = dot_product(numpy.transpose(cstar),modMatInv(numpy.transpose(pstar),len(gamma))) #da dispense
+            key = dot_product(numpy.transpose(cstar),modMatInv(numpy.transpose(pstar),len(gamma))) #as in textbook, matrix shall be transposed wrt to the one computed yet
             print("!!FOUND THE KEY!!")
             return key
         except:
-            shiftLeft(pairs) #provo a vedere che chiave riesco a ottenere shiftando a sinistra il vettore delle coppie, aggiornando quindi P* e C*
+            shiftLeft(pairs) #Shift left vector of pairs plaintext,ciphertext and the next iteration will refresh P* and C*
             k = k + 1
     return None
 
-def key2str(key):
+def key2str(key): #utility function which prints out a matrix
     k2s = []
     for row in key:
         k2srow = ''
@@ -73,20 +142,22 @@ def key2str(key):
         k2s.append(k2srow)
     return str(k2s).replace(",","\r\n").replace("[","\r\n").replace("]","")
 
-if __name__ == '__main__':
-    txt = clean("Il cifrario di Hill risulta vulnerabile ad attacchi di tipo known plaintext")
+if __name__ == '__main__': #main code
+    txt = clean("Il cifrario di Hill risulta vulnerabile ad attacchi di tipo known plaintext") #example of a plaintext
     key = [ [1,2,3,2]
            ,[1,1,4,3]
            ,[1,1,1,7]
-           ,[1,2,1,1] ]
-    ciphertext = encrypt(txt,key)
-    print("Input text: "+txt)
-    print("Ciphertext: "+ciphertext)
-    decrypted = decrypt(ciphertext,key)
+           ,[1,2,1,1] ] #example of a 4 x 4 key
+    ciphertext = encrypt(txt,key) #encryption of ciphertext
+    print("Input text: "+txt) #print input text
+    print("Ciphertext: "+ciphertext) #print ciphertext
+    decrypted = decrypt(ciphertext,key) #print regular decryption assuming that we know the key
     print("Decrypted: "+decrypt(ciphertext,key))
     print("*********** ATTACKING HILL CIPHER ***********")
-    guessed_key = find_key(txt,ciphertext,len(key))
-    if guessed_key is not None:
-        print("Decryption of ciphertext using the guessed key:" + key2str(guessed_key))
+    guessed_key = find_key(txt,ciphertext,len(key)) #call crack hill routine
+    if guessed_key is not None: #if the key is found, then:
+        print("Decryption of ciphertext using the guessed key:" + key2str(guessed_key)) #test decryption
         print(decrypt(ciphertext,guessed_key))
+    else:
+        print("No suitable pairs of plaintext,ciphertext")
 
--- Set1/mgram.py
+++ Set1/mgram.py
@@ -1,102 +1,114 @@
-import math
+#This is the code of Exercise 3.1
+#It needs libraries scipy, numpy and matplotlib, both available with pip: launch pip install (library name) if any is missing
+#In order to execute the code, user has to launch python3 mgram.py [pathToFile] where [pathToFile] is the path of the file containing the text to be analyzed [e.g mobydick.txt]
+#
+#Author: Alex Foglia, 6336805
 
-gamma = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-english_freq = [8.167,1.492,2.782,4.253,12.702,2.228,2.015,6.094,6.966,0.153,0.772,4.025,2.406,6.749,7.507,1.929,0.095,5.987,6.327,9.056,2.758,0.978,2.360,0.150,1.974,0.074]
+import math                                                         #Library used to compute logarithm
+from scipy import stats                                             #Libraries used to print a distribution overview
+import numpy as np
 
-def clean(text):
-    newstr = text.lower()
-    for ch in newstr:
+gamma = ['a','b','c','d','e','f','g','h','i','j','k','l',\
+        'm','n','o','p','m','r','s','t','u','v','w','x','y','z']    #Defined alphabet a priori   
+                                        
+english_freq = [8.167,1.492,2.782,4.253,12.702,2.228,2.015,\
+        6.094,6.966,0.153,0.772,4.025,2.406,6.749,7.507,1.929,\
+        0.095,5.987,6.327,9.056,2.758,0.978,2.360,0.150,1.974,0.074]#English letter frequency vector
+
+def clean(text):                                                    #Routine used to clean the text as specified
+    newstr = text.lower()                                           #Convert string to lowercase
+    for ch in newstr:                                               #Foreach character in the text, if it is not included in gamma, replace it
         if ch not in gamma:
             newstr = newstr.replace(ch,'')
-    return newstr
+    return newstr                                                   #Returning clean text to caller
     
-def fi(letter,text):
+def fi(string,text):                                                #Routine used to count the occurences of a given string in a given text
     c = 0
     for i in range(0,len(text)):
-        if text[i] == letter:
+        if text[i] == string:
             c = c + 1
     return c
 
-def c_index(text):
-    index = .0
-    text = clean(text)
-    n = len(text)
-    for f in gamma:
-        _fi = fi(f,text)
-        ratio = (_fi*(_fi - 1))/(n*(n-1))
-        index = index + ratio
-    return index
 
-def all_qgrams(q, text):
-    qgrams = []
+def all_mgrams(m, text):                                            #Routine used to compute all m grams in a given text
+    mgrams = []
     i = 0
     while i < len(text):
-        next_qgram = text[i:i+q]
-        if not next_qgram in qgrams:
-            qgrams.append(next_qgram)
-        i = i + q
-    return qgrams
+        next_mgram = text[i:i+m]                                    #Substring of interest
+        if next_mgram not in mgrams:
+            mgrams.append(next_mgram)
+        i = i + m
+    return mgrams                                                   #Returning all m grams to caller
 
-def qgram_count(qgrams, text):
-    q = len(qgrams[0])
-    count = dict()
-    for qgram in qgrams:
-        i = 0
-        n = 0
-        while i < len(text):
-            if text[i:i+q] == qgram:
-                n = n + 1
-            i = i + q
-        count[qgram] = n
-    return count
+def mgram_count(mgrams, text):                                      #Routine used to count all mgram occurences in a given text
+    m = len(mgrams[0])                                              #Compute m
+    count = dict()                                                  #Dictionary initialization
+    size = len(text)
+    for mgram in mgrams:                                            #Foreach mgram count its frequency and assign it to dictionary[mgram]
+        _text = text.replace(mgram,"")
+        delta = size - len(_text)
+        count[mgram] = delta/m                                           
+    return count                                                    #Returning dictionary to caller
 
-def plot_dictionary(q,d,title):
-    import matplotlib.pyplot as plt
-    keys = list(d.keys())
+def plot_dictionary(d,title="Letter frequency in provided text"):   #Routine used to plot a dictionary representing a distribution as histogram
+    import matplotlib.pyplot as plt                                 #Import matplotlib.pyplot library in order to being able to plot a dictionary
+    keys = list(d.keys())                                           #Rearrange keys in alphabetical order
     keys.sort()
     values = []
-    for key in keys:
+    for key in keys:                                                #Ordering the distribution
         values.append(d[key])
-    plt.bar(keys,values,color = 'b')
-    plt.title(str(q)+title)
+    plt.bar(keys,values,color = 'b')                                #Construct and show histogram
+    plt.title(title)
     plt.show()
 
-def exit_with_prompt(prompt):
-    print(prompt)
-    exit()
-
-def shannon_h(qgrams):
-    h_dict = dict()
-    for A in qgrams:
-        h = 0
-        for a in A:
-            fa = float(fi(a,A))
-            h = h + (fa/float(len(A)))*math.log2(fa/float(len(A)))
-        h_dict[str(A)] = -h
-    print(h_dict)
-    return h_dict
-
-if __name__ == '__main__':
-    import sys
-    txt = ""
+def __readFile__():                                                 #Routine used to read the input file provided as argument
+    import sys                                                      #Import of sys module in order to being able to parse CLI arguments
     try:
-        txt = ''.join(open(sys.argv[1],'r+').readlines())
-        txt = clean(txt)
+        txt = ''.join(open(sys.argv[1],'r+').readlines())           #Constructing the string reading all lines in provided file
+        txt = clean(txt)                                            #Cleaning text as requested
+        return txt                                                  #Returning read text to caller
     except:
-        exit_with_prompt("No text provided as argument, exiting . . .")
-    print("Read string: "+txt+"\n")
-    print("C index = " + str(c_index(txt)))
-    try:
-        q = int(input("Insert max m\n"))
-    except:
-        exit_with_prompt("Not a valid integer, exiting . . .")
-    if q>0:
-        for i in range(1,q+1):
-            qgrams = all_qgrams(i, txt)
-            for qgram in qgrams:
-                if len(qgram)>1:
-                    print("c index of "+qgram+" = "+str(c_index(qgram)))
-            #plot_dictionary(i,shannon_h(qgrams),"-gram H distribution")
-            #count = qgram_count(qgrams, txt)
-             #plot_dictionary(i,count,"-gram frequencies")
+        raise Exception("No valid file provided as argument")       #If something went wrong, raise an Exception
+
+def __hist__(txt):                                                  #Routine used to plot histogram of each letter frequency
+    d = {}                                                          #Dictionary initialization
+    for letter in gamma:                                            #Foreach letter in the alphabet
+        d[letter] = float(fi(letter,txt))/len(txt)                  #d[letter] is the frequency of each letter in the text divided by the length of the text
+    plot_dictionary(d)                                              #Plot of the histogram and print distribution
+
+def entropy_dic(d):                                                 #Routine used to calculate Shannon Entropy of a given dictionary of a mgram frequency distribution
+    n = sum(d.values())                                             #Applying formula
+    entropy = 0     
+    for key in d.keys():
+        entropy += d[key] / n * math.log2(d[key] / n)
+    return (-1) * (entropy) / math.log2(len(d.keys()))
+
+def c_index(d):                                                     #Routine used to calculate coincide index of a given dictionary of a mgram frequency distribution
+    cIndex = 0.0                                                    #Applying formula
+    n = float(sum(d.values()))                      
+    for key in d.keys():
+        cIndex += float(d[key])*float(d[key]-1)
+    return cIndex / ((n)*n-1)
+
+def __distribSummary__(m, d):                                       #Routine used to print a summary of the distribution of mgrams
+    d = stats.describe(np.array(list(d.values())))
+    print("{}".format(d).replace("DescribeResult","Distribution of {}-grams summary: ".format(m)))
+
+#-----------------------START DRIVER CODE---------------------------#
+if __name__ == '__main__':                                              #Check if the script is called directly
+    txt = __readFile__()                                                #Reading input file (e.g. mobydick.txt)
+    __hist__(txt)                                                       #Plot histogram of relative letter frequencies in parsed text
+    m = 4                                                               #Maximum m
+    for j in range(1,m+1):                                              #For mgrams with m from 1 to 4                                            
+        mgrams = all_mgrams(j, txt)                                     #Get all m grams
+        c = mgram_count(mgrams, txt)                                    #Count all absolute mgrams frequencies distribution
+        cr = {}                                                         #Construct relative mgrams frequencies distribution
+        for key in c.keys():
+            cr[key] = float(c[key])/(float(len(txt))/j)
+        __distribSummary__(j,cr)
+        cIndex = c_index(c)                                             #Compute and print coincidence index of mgram distribution
+        print("C-index of {}-grams distribution = {}".format(j,cIndex))
+        h = entropy_dic(c)                                              #Compute and print entropy of mgram distribution
+        print("Entropy of {}-grams distribution = {}".format(j,h))
+#-----------------------END DRIVER CODE-----------------------------#
 
