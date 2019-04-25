8334a51111ccb1fe45819ad87ba9e558f2178dee
Alex Foglia <alex.f1793@gmail.com>
2019-04-25 17:50:45 +0200

    load from file done

--- Set2/Es32.py
+++ Set2/Es32.py
@@ -25,37 +25,49 @@ def var(v):
         copy.append((mean-vi)**2)
     return sum(copy)/len(copy)
 
-def test():
+def test(filename = None, size = 100):
+    n = []
     pq = []
-    print("Generating p,q . . .")
-    for i in range(0,10):
-        pq.append((randPrime(10**150,10**151,10),randPrime(10**150,10**151,10)))
-        print("{}-th step done".format(i))
     de = []
-    for pair in pq:
-        p = pair[0]
-        q = pair[1]
-        print("Generated n = {}".format(p*q))
-        priv,pub = rsaKeyGen(p,q)
-        print("Generated d = {}".format(priv[0]))
-        print("Generated e = {}".format(pub[0]))
-        de.append((priv[0],pub[0]))
+    if filename == None:
+        #generate n,p,q,d,e
+        print("Generating p,q . . .")
+        for i in range(0,size):
+            pq.append((randPrime(10**150,10**151,10),randPrime(10**150,10**151,10)))
+            print("{}-th step done".format(i+1))
+        for pair in pq:
+            p = pair[0]
+            q = pair[1]
+            _n = p*q
+            n.append(_n)
+            print("Generated n = {}".format(_n))
+            priv,pub = rsaKeyGen(p,q)
+            print("Generated d = {}".format(priv[0]))
+            print("Generated e = {}".format(pub[0]))
+            de.append((priv[0],pub[0]))
+    else:
+        #load n,d,e from provided csv file
+        f = open(filename,'r')
+        for line in f.readlines():
+            split = line.split(',')
+            n.append(int(split[0]))
+            de.append((int(split[1]),int(split[2])))
     it = []
     times = []
-    for i in range(0,10):
-        n = pq[i][0]*pq[i][1]
+    for i in range(0,size):
+        _n = n[i]
         d = de[i][0]
         e = de[i][1]
-        print("Testing on n = {} d = {} e = {}".format(n,d,e))
+        print("Testing on n = {} d = {} e = {}".format(_n,d,e))
         t0 = t.time()
-        res = decryptionexp(n,d,e)
+        res = decryptionexp(_n,d,e)
         delta_t = t.time()-t0
         it.append(res[1])
         times.append(delta_t)
         print("Iterations: {}".format(res[1]))
         print("Elapsed time: {} sec".format(delta_t))
-    avg_it = sum(it)/10
-    avg_time = sum(times)/10
+    avg_it = sum(it)/size
+    avg_time = sum(times)/size
     print("****** Results ******")
     print("Avg it\tAvg time (s)\tVar time (s^2)")
     print("{}\t{}\t{}".format(avg_it,avg_time,var(times)))
@@ -81,6 +93,7 @@ def generateToFile(filename, size = 100):
         i = i + 1
     f.close()
 if __name__ == '__main__':
-    generateToFile("nde.txt")
+    #generateToFile("nde.txt",100)
+    test("nde.txt",100)
     
             
